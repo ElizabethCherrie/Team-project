f27dee3043de5cf05602c2af93bc15209d1d2ad1
Nizar Omar <Nizar.Omar@city.ac.uk>
2026-02-24 23:10:31 +0000

    Added 2 coding files from the component diagram
    added the 2 interfaces

---
+++ IPOS-SA/src/I_IPOS_PU.java
@@ -0,0 +1,26 @@
+// public interface I_IPOS_PU {
+	/**
+     *
+	 * @param sender
+	 * @param receivers
+	 * @param subject
+ 	 * @param body
+	 */
+	//void sendEmail(string sender, string[] receivers, string subject, string body);
+
+	/**
+	 *
+	 * @param currency
+	 * @param amount
+	 * @param cardDetails
+	 */
+	//int processPayment(char currency, float amount, Card cardDetails);
+
+	/**
+	 * 
+	 * @param orderID
+	 * @param newStatus
+	 */
+	//void updateOrderStatus(int orderID, string newStatus);
+
+// }
